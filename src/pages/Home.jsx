0476319208ac759b17602736641edfe96777553c
Serhii <sirozhka.v@gmail.com>
2023-03-02 17:31:12 +0100

    add layout to Cart

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,9 +14,10 @@ const Home = () => {
       setItems(res.data);
       setIsLoading(false);
     });
+    window.scrollTo(0, 0);
   }, []);
   return (
-    <>
+    <div className="container">
       <div className="content__top">
         <Categories />
         <Sort />
@@ -27,7 +28,7 @@ const Home = () => {
           ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
           : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
       </div>
-    </>
+    </div>
   );
 };
 
