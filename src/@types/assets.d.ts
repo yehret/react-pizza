85669d46e8110bbd6b3b05eec44f3c9c29f17582
Serhii <sirozhka.v@gmail.com>
2023-03-16 16:24:35 +0100

    create custom types, typing Header component

---
+++ src/@types/assets.d.ts
@@ -0,0 +1,41 @@
+declare module '*.svg' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.png' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.scss' {
+  const content: any;
+  export default content;
+}
+
+
+  // declare module '*.jpg' {
+  //   const src: string;
+  //   export default src;
+  // }
+  
+  // declare module '*.png' {
+  //   const src: string;
+  //   export default src;
+  // }
+  
+  // declare module '*.svg' {
+  //   import * as React from 'react';
+  
+  //   export const ReactComponent: React.FunctionComponent<React.SVGProps<
+  //       SVGSVGElement
+  //       > & { title?: string }>;
+  
+  //   const src: string;
+  //   export default src;
+  // }
+  
+  // declare module '*.module.css' {
+  //   const classes: { readonly [key: string]: string };
+  //   export default classes;
+  // }
